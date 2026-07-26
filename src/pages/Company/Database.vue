674f1535ef9799db90fed99480c6c7f92eb8aa69
zb <345692197@qq.com>
2026-07-26 20:28:27 +0800

    1

--- src/pages/Company/Database.vue
+++ src/pages/Company/Database.vue
@@ -230,7 +230,7 @@ const columns = [
         'span',
         {
           style: { color: '#0052d9', cursor: 'pointer' },
-          onClick: () => openSourceModal(row.parentCompanyId!),
+          onClick: () => openSourceModal(row.id!),
         },
         row.cnt || 0,
       ),
